a6beeeec354c49c38ce0ba479bb3e9493b5b6f9f
Victom07 <98037605+Victom07@users.noreply.github.com>
2022-02-19 19:09:07 +0000

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,18 @@
+function getnum(num){
+    console.log(num);
+    var result=document.getElementById("result");
+    result.value+=num;
+}
+
+function Clear(){
+  var result = document.getElementById("result");  
+  result.value =" ";  
+
+
+}
+
+function getResult(){
+    var result = document.getElementById("result");  
+    result.value = eval(result.value);
+    
+}
